f6d21dc9298a42ce08235bb05bf426d27ef958fe
s0cm0nkey <74802544+s0cm0nkey@users.noreply.github.com>
2023-03-31 14:55:42 -0400

    Create OpenAIExplain.py

---
+++ OpenAIExplain.py
@@ -0,0 +1,15 @@
+import openai 
+
+openai.api_key = "API_KEY_HERE" 
+
+inp = input('Paste Command Line Text\n') 
+
+prompt = "Explain the following command string:" + inp 
+
+response = openai.Completion.create(model="text-davinci-003", 
+
+prompt=prompt, temperature=0.7, max_tokens=3000) 
+
+print("Command Explanation") 
+
+print(response['choices'][0]['text']) 
